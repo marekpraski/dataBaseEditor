b985f246653c5b823ad0c9ecf6340fab6f03f12d
Marek Praski <marekpraski@wp.pl>
2019-08-20 10:45:07 +0200

    poprawione błędy

--- DataBaseEditor/CellConverter.cs
+++ DataBaseEditor/CellConverter.cs
@@ -13,34 +13,104 @@ namespace DataBaseEditor
     {       
         public bool verifyCellDataType(ref DataGridCell cell)
         {
-            // rozważam bazodanowe typy danych, w grupach: (bit, int, bigint), (char, varchar), (float, decimal, numeric), (datetime), (geometry)
+            // rozważam bazodanowe typy danych:  bit, int, bigint, (char, varchar), w grupach: (float, decimal, numeric), (datetime), (geometry)
             string typeName = cell.DataTypeName;
-            if (typeName.Contains("bit") || typeName.Contains("int"))
+            object cellValue = cell.getCellValue(cellValueTypes.newValue);
+            if (cellValue != null)
             {
-                return handleBigint(cell);
+                if (typeName.Contains("bigint"))
+                {
+                    return handleBigint(cellValue);
+                }
+                else if (typeName.Contains("bit"))
+                {
+                    return handleBit(cellValue);
+                }
+                else if (typeName.Contains("int"))
+                {
+                    return handleInt(cellValue);
+                }
+                else if (typeName.Contains("char"))
+                {
+                    return true;
+                }
+                else if (typeName.Contains("float") || typeName.Contains("decimal") || typeName.Contains("numeric"))
+                {
+                    return handleDouble(cellValue);
+                }
+                else if (typeName.Contains("datetime"))
+                {
+                    return handleDatetime(cellValue);
+                }
+                else if (typeName.Contains("geometry"))
+                {
+                    MyMessageBox.display("Nie można edytować pól typu geometry", MessageBoxType.Error);
+                }
+                return false;
             }
-            else if (typeName.Contains("char"))
+            return true;
+        }
+
+        private bool handleInt(object cellValue)
+        {
+            try
             {
+                int value = int.Parse(cellValue.ToString());    //zwykły parse zapisany jako  (int)cellValue  nie działa
                 return true;
             }
-            else if (typeName.Contains("float") || typeName.Contains("decimal") || typeName.Contains("numeric"))
+            catch (FormatException e)
             {
-                return handleDouble(cell);
+                MyMessageBox.display(e.Message + "\r\nNależy wprowadzić liczbę całkowitą", MessageBoxType.Error);
             }
-            else if (typeName.Contains("datetime"))
+            catch (OverflowException e)
             {
-                return handleDatetime(cell);
-            }
-            else if (typeName.Contains("geometry"))
-            {
-                MyMessageBox.display("Nie można edytować pól typu geometry", MessageBoxType.Error);
+                MyMessageBox.display(e.Message + "\r\nNależy wprowadzić liczbę całkowitą od -32,768 do 32,767 ", MessageBoxType.Error);
             }
             return false;
         }
 
-        private bool handleDatetime(DataGridCell cell)
+        private bool handleBit(object cellValue)
         {
-            object cellValue = cell.getCellValue(cellValueTypes.newValue);
+            try
+            {
+                int value = int.Parse(cellValue.ToString());
+                if (value == 1 || value == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    MyMessageBox.display("\r\nNależy wprowadzić liczbę 0 lub 1", MessageBoxType.Error);
+                    return false;
+                }
+            }
+            catch (FormatException e)
+            {
+                MyMessageBox.display(e.Message + "\r\nNależy wprowadzić liczbę 0 lub 1", MessageBoxType.Error);
+            }
+            catch (OverflowException e)
+            {
+                MyMessageBox.display(e.Message + "\r\nNależy wprowadzić liczbę 0 lub 1", MessageBoxType.Error);
+            }
+            return false;
+        }
+
+        private bool handleBigint(object cellValue)
+        {
+            try
+            {
+                long value = long.Parse(cellValue.ToString());
+                return true;
+            }
+            catch (FormatException e)
+            {
+                MyMessageBox.display(e.Message + "\r\nNależy wprowadzić liczbę całkowitą", MessageBoxType.Error);
+            }
+            return false;
+        }
+
+        private bool handleDatetime(object cellValue)
+        {
             try
             {
                 DateTime value = (DateTime)cellValue;
@@ -53,9 +123,8 @@ namespace DataBaseEditor
             return false;
         }
 
-        private bool handleDouble(DataGridCell cell)
+        private bool handleDouble(object cellValue)
         {
-            object cellValue = cell.getCellValue(cellValueTypes.newValue);
             string stringCellValue = cellValue.ToString(); 
             try
             {
@@ -72,22 +141,7 @@ namespace DataBaseEditor
             }
             return false;
         }
-
-        private bool handleBigint(DataGridCell cell)
-        {
-            object cellValue = cell.getCellValue(cellValueTypes.newValue);
-            try
-            {
-                long value = (long)cellValue;
-                return true;
-            }
-            catch (InvalidCastException e)
-            {
-                MyMessageBox.display(e.Message + "\r\nNależy wprowadzić liczbę całkowitą", MessageBoxType.Error);
-            }
-            return false;
-        }
-
+        
         //skoro wartość przeszła pierwsze sprawdzenie funkcją verifyCellDataType to ta funkcja musi rozważyć tylko trzy przypadki:
         //czy zwrócona wartość nie jest nullowa (użytkownik skasował zawartość celki)
         //zwrócić string w apostrofach i zamienić przecinek na kropkę w double
